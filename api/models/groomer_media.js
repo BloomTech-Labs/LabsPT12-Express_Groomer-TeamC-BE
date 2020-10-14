cc14ccbf5a0e3ae12f7a7bed8671321a0adec4cd
danielkanangila <daniel.kanangila@gmail.com>
2020-10-14 11:51:22 -0400

    added groomer media model

---
+++ api/models/groomer_media.js
@@ -0,0 +1,23 @@
+const Model = require('./Model');
+
+class GroomerMedia extends Model {
+  constructor() {
+    super();
+    this.tableName = 'groomer_medias';
+
+    this.validationSchema = {
+      type: 'object',
+      required: ['groomer_id', 'url'],
+      properties: {
+        groomer_id: {
+          type: 'string',
+          oneOf: { key: 'id', target: 'groomers' },
+        },
+        url: { type: 'string' },
+        description: { type: 'string' },
+      },
+    };
+  }
+}
+
+module.exports = new GroomerMedia();
