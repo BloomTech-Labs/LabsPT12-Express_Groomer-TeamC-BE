ee8ffb82be218fe28006c8806c900cf3d839fcc4
danielkanangila <daniel.kanangila@gmail.com>
2020-10-14 12:14:56 -0400

    added animal model

---
+++ api/models/animal.js
@@ -0,0 +1,24 @@
+const Model = require('./Model');
+
+class Animal extends Model {
+  constructor() {
+    super();
+    this.tableName = 'animals';
+
+    this.validationSchema = {
+      type: 'object',
+      required: ['owner_id', 'name', 'animal_type', 'breed'],
+      properties: {
+        owner_id: { type: 'string', oneOf: { key: 'id', target: 'profile' } },
+        name: { type: 'string' },
+        animal_type: { type: 'string' },
+        breed: { type: 'string' },
+        weight: { type: 'string' },
+        picture: { type: 'string' },
+        comment: { type: 'string' },
+      },
+    };
+  }
+}
+
+module.exports = new Animal();
