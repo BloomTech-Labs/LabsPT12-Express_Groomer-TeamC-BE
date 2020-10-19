803088c7f824706c4b68edb92064d36b6f980b9a
danielkanangila <daniel.kanangila@gmail.com>
2020-10-19 12:36:51 -0400

    fixed lint issue in groomer_service migration

--- data/seeds/007_groomer_service.js
+++ data/seeds/007_groomer_service.js
@@ -1,7 +1,8 @@
 const faker = require('faker');
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('groomer_services').del()
+  return knex('groomer_services')
+    .del()
     .then(function () {
       // Inserts seed entries
       return knex('groomer_services').insert([
@@ -10,7 +11,7 @@ exports.seed = function(knex) {
           groomer_id: '00ulthapbErVUwVJt48x7',
           service_id: '11ulthapbErVUwVJt58x0',
           service_hours: 'Mon-Fri 9am - 5pm',
-        }
+        },
       ]);
     });
 };
