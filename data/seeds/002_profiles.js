65da87723f702428098ef319b6731b3c11648e2d
Lydie Cherilus <lydiecherilus@yahoo.fr>
2020-10-22 22:12:05 -0400

    remove random users and update seed

--- data/seeds/002_profiles.js
+++ data/seeds/002_profiles.js
@@ -1,27 +1,68 @@
 const faker = require('faker');
 
-const userTypeIds = [
-  '035f3a60-0de0-11eb-93e6-ddb47fc994e4',
-  'dc885650-0de0-11eb-8250-a5697c93ae91',
-];
-
-const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
-  user_type: userTypeIds[Math.round(Math.random())],
-  avatarUrl: faker.image.avatar(),
-  email: idx === 0 ? 'llama001@maildrop.cc' : faker.internet.email(),
-  name:
-    idx === 0
-      ? 'Test001 User'
-      : `${faker.name.firstName()} ${faker.name.lastName()}`,
-}));
-
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('profiles')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('profiles').insert(profiles);
+      return knex('profiles').insert([
+        {
+          id: '00ulthapbErVUwVJy4x6',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama001@maildrop.cc',
+          name: 'Kole Hane',
+          user_type: 'dc885650-0de0-11eb-8250-a5697c93ae91',
+        },
+        {
+          id: 'user2_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama002@maildrop.cc',
+          name: 'Patricia King',
+          user_type: 'dc885650-0de0-11eb-8250-a5697c93ae91',
+        },
+        {
+          id: 'user3_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama003@maildrop.cc',
+          name: 'Garland Baumbach',
+          user_type: 'dc885650-0de0-11eb-8250-a5697c93ae91',
+        },
+        {
+          id: 'user4_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama004@maildrop.cc',
+          name: 'Arvel Bernier',
+          user_type: 'dc885650-0de0-11eb-8250-a5697c93ae91',
+        },
+        {
+          id: 'user5_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama005@maildrop.cc',
+          name: 'Daphney Goldner',
+          user_type: 'dc885650-0de0-11eb-8250-a5697c93ae91',
+        },
+        {
+          id: 'user6_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama006@maildrop.cc',
+          name: 'Tim Leon',
+          user_type: 'dc885650-0de0-11eb-8250-a5697c93ae91',
+        },
+        {
+          id: 'user7_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama007@maildrop.cc',
+          name: 'Martin Mark',
+          user_type: '035f3a60-0de0-11eb-93e6-ddb47fc994e4',
+        },
+        {
+          id: 'user8_id',
+          avatarUrl: faker.image.avatar(),
+          email: 'llama008@maildrop.cc',
+          name: 'Mary Love',
+          user_type: '035f3a60-0de0-11eb-93e6-ddb47fc994e4',
+        },
+      ]);
     });
 };
--- data/seeds/003_groomer.js
+++ data/seeds/003_groomer.js
@@ -11,6 +11,36 @@ exports.seed = function (knex) {
           travel: true,
           travel_distance: 50,
         },
+        {
+          id: 'profile2_id',
+          profile_id: 'user2_id',
+          travel: false,
+          travel_distance: 0,
+        },
+        {
+          id: 'profile3_id',
+          profile_id: 'user3_id',
+          travel: false,
+          travel_distance: 0,
+        },
+        {
+          id: 'profile4_id',
+          profile_id: 'user4_id',
+          travel: true,
+          travel_distance: 20,
+        },
+        {
+          id: 'profile5_id',
+          profile_id: 'user5_id',
+          travel: false,
+          travel_distance: 0,
+        },
+        {
+          id: 'profile6_id',
+          profile_id: 'user6_id',
+          travel: true,
+          travel_distance: 30,
+        },
       ]);
     });
 };
