c80e5a408c653ccf50f4de22437e5d6b2c252ba9
danielkanangila <daniel.kanangila@gmail.com>
2020-11-06 19:35:16 -0500

    added payment history model

---
+++ api/models/payment_history.js
@@ -0,0 +1,28 @@
+const Model = require('./Model');
+
+class PaymentHistory extends Model {
+  constructor() {
+    super();
+    this.tableName = 'payment_histories';
+
+    this.validationSchema = {
+      type: 'object',
+      required: ['client_id', 'groomer_id', 'service_id', 'amount'],
+      properties: {
+        client_id: { type: 'string', oneOf: { key: 'id', target: 'profiles' } },
+        groomer_id: {
+          type: 'string',
+          oneOf: { key: 'profile_id', target: 'groomers' },
+        },
+        service_id: {
+          type: 'string',
+          oneOf: { key: 'id', target: 'groomer_services' },
+        },
+        amount: { type: 'number' },
+        payment_method: { type: 'string' },
+      },
+    };
+  }
+}
+
+module.exports = new PaymentHistory();
