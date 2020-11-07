86afceabae43b88336614592bddce07dbae54908
danielkanangila <daniel.kanangila@gmail.com>
2020-11-07 16:55:32 -0500

    added stripe

--- api/payment/paymentRepository.js
+++ api/payment/paymentRepository.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Repository = require('./../models/Repository');
 const PaymentHistory = require('./../models/payment_history');
 
@@ -5,6 +6,28 @@ class PaymentRepository extends Repository {
   constructor() {
     super();
     this.model = PaymentHistory;
+  }
+
+  async beforeCreate(payload) {
+    const { stripeToken, ...cart } = payload;
+
+    const customer = await stripe.customers.create({
+      email: require.profile.email,
+      source: stripeToken,
+    });
+
+    const charge = await stripe.charges.create({
+      amount: cart.amount,
+      description: 'Service',
+      currency: 'usd',
+      customer: customer.id,
+    });
+
+    console.log(customer);
+
+    console.log(charge);
+
+    return cart;
   }
 
   async afterCreate(result) {
--- api/payment/paymentRouter.js
+++ api/payment/paymentRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const authRequired = require('./../middleware/authRequired');
+const permissions = require('./../middleware/permissions');
 const PaymentController = require('./paymentController');
 
 router.post(
   '/charge',
   authRequired,
+  permissions.canMakePayment,
   PaymentController.post.bind(PaymentController)
 );
 
